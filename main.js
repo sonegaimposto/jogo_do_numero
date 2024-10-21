3d8eb11f7fad1549f674ce2f1893e85c3b5d4fdb
raffico <raffael.vieira@escola.pr.gov.br>
2024-10-21 11:29:30 -0300

    .

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
 let contagemPalpites = 1;
 let botaoReiniciar;
+
+function verificarPalpite() {
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagem === 1) {
+        palpites.textContent = "Palpites anteriores: ";
+    }
+}
+
+palpites.textContent += palpiteUsuario + " ";
+
+if (palpiteUsuario === numeroAleatorio) {
+    ultimoResultado.textContent = "vosê acerto disgrasadooo!";
+    ultimoResultado.style.backgroundColor = "green";
+    baixoOuAlto.textContent = "";
+    finalizarJogo();
+} else if (contagemPalpites === 10) {
+    ultimoResultado.textContent = `perdeu otario '-'`;
+    baixoOuAlto.textContent = "";
+    finalizarJogo();
+} else {
+    ultimoResultado.textContent = "Errado pora";
+    ultimoResultado.style.backgroundColor = "red";
+    if (palpiteUsuario < numeroAleatorio) {
+        baixoOuAlto.textContent = "ta baixo nengue";
+    } else if (ultimoResultado > numeroAleatorio) {
+        baixoOuAlto.textContent = "ta mais alto nengue";
+    }
+}
