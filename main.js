adac682cff4c2be07730d01000936abc71f7edea
raffico <raffael.vieira@escola.pr.gov.br>
2024-10-22 08:46:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -49,6 +49,19 @@ function finalizarJogo() {
     botaoReiniciar.addEventListener('click', reiniciarJogo());
 }
 
+
 function reiniciarJogo() {
-    
+    contagemPalpites = 1;
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
+    for (const paragrafoReiniciar of paragrafosReiniciar) {
+        paragrafoReiniciar.textContent = "";
+    }
+
+    botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+    campoPalpite.disabled = false;
+    envioPalpite.disabled = false;
+    campoPalpite.value = "";
+    campoPalpite.focus();
+    ultimoResultado.style.backgroundColor = 'white';
+    numeroAleatorio = Math.floor(Math.random * 100) + 1
 }
